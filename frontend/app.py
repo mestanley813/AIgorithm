f01d4b3bc50b7cc9c25093d5d6c3b0c4ddefb6e4
Morgan <mestanley813@gmail.com>
2023-05-15 12:14:20 -0400

    Update database import

--- frontend/app.py
+++ frontend/app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template
 from backend import database, purchase
 
+db = database.DB()
 
 def getBalance():
     acc = purchase.Account().api.get_account()
-    return round(float (acc.equity) - float(acc.last_equity), 2)
+    balance = round(float (acc.equity) - float(acc.last_equity), 2)
+    return abs(balance)
+
 def getBought():
-    return database.database().collection.find({}).sort("date_of_purchase", -1).limit(1)[0]["symbol"]
+    return db.collection.find({}).sort("date_of_purchase", -1).limit(1)[0]["symbol"]
+
 
 def getSold():
-    return database.database().collection.find({}).sort("date_of_purchase", -1).limit(2)[1]["symbol"]
+    return db.collection.find({}).sort("date_of_purchase", -1).limit(2)[1]["symbol"]
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", balance_change=getBalance(), buy=getBought(), sell=getSold())
+    stocks = db.collection.find().sort("date_of_purchase", -1).limit(9)
+
+    return render_template("index.html", balance_change=getBalance(), buy=getBought(), sell=getSold(), stocks = stocks)
